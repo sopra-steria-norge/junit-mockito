5d46f6bb6992c5b775288aab49677a64649b8387
teide <tee@soprasteria.com>
2017-04-02 18:23:35 +0200

    Oppgave 2

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 Verifikasjon av interaksjoner
 
-## Oppgave 3: Mockito.verify()
+## Oppgave 2: Mockito.doThrow()
 
-Det skal kun sendes vedtak dersom beløpet er > 0. Lag en test som verifiserer 
-at det sendes vedtak når beløpet er > 0 og at det ikke sendes vedtak når beløpet er 0. 
+Dersom 
+
 
 
 
--- src/test/java/com/soprasteria/vedtak/VedtakServiceTest.java
+++ src/test/java/com/soprasteria/vedtak/VedtakServiceTest.java
@@ -13,16 +13,7 @@ public class VedtakServiceTest {
 
     @Test
     public void sender_digital_post_naar_vedtak_godkjennes() {
-        DigitalPostKlient digitalPostKlient = mock(DigitalPostKlient.class);
-        UtbetalingBeregner utbetalingBeregner = mock(UtbetalingBeregner.class);
-        VedtakService vedtakService = new VedtakService(digitalPostKlient, utbetalingBeregner);
 
-        Long kundeId = 5L;
-        when(utbetalingBeregner.beregnUtbetalingFor(kundeId)).thenReturn(BigDecimal.TEN);
-
-        vedtakService.godkjennVedtak(kundeId);
-
-        verify(digitalPostKlient).sendVedtaksbrev(BigDecimal.TEN);
     }
 
 }
