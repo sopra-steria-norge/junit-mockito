de2569546f5921e48ece67e10bd9a50e0132488a
teide <tee@soprasteria.com>
2017-04-02 12:20:46 +0200

    Oppgave6

--- src/main/java/com/soprasteria/vedtak/Vedtaksbrev.java
+++ src/main/java/com/soprasteria/vedtak/Vedtaksbrev.java
@@ -1,7 +1,6 @@
 package com.soprasteria.vedtak;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
 public class Vedtaksbrev {
     private final String navn;
@@ -20,17 +19,13 @@ public class Vedtaksbrev {
         return belop;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Vedtaksbrev that = (Vedtaksbrev) o;
-        return Objects.equals(navn, that.navn) &&
-                Objects.equals(belop, that.belop);
-    }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(navn, belop);
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Vedtaksbrev{");
+        sb.append("navn='").append(navn).append('\'');
+        sb.append(", belop=").append(belop);
+        sb.append('}');
+        return sb.toString();
     }
 }
