453bf5e4722421b56cd824fb67d366298f41467c
teide <tee@soprasteria.com>
2017-04-02 19:22:39 +0200

    Løsning oppgave 6

--- src/test/java/com/soprasteria/vedtak/VedtakServiceTest.java
+++ src/test/java/com/soprasteria/vedtak/VedtakServiceTest.java
@@ -6,7 +6,6 @@ import com.soprasteria.com.soprasteria.kunde.Kunde;
 import com.soprasteria.com.soprasteria.kunde.KundeRepository;
 import com.soprasteria.digitalpost.DigitalPostKlient;
 import org.junit.Test;
-import org.mockito.ArgumentMatcher;
 import org.mockito.Mockito;
 
 import java.math.BigDecimal;
@@ -14,31 +13,44 @@ import java.util.Objects;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 public class VedtakServiceTest {
 
+    private DigitalPostKlient digitalPostKlient = mock(DigitalPostKlient.class);
+    private UtbetalingBeregner utbetalingBeregner = mock(UtbetalingBeregner.class);
+    private KundeRepository kundeRepository = mock(KundeRepository.class);
+    private VedtakService vedtakService = new VedtakService(digitalPostKlient, utbetalingBeregner, kundeRepository);
+    private Long kundeId = 5L;
+
+
     @Test
     public void sender_digital_post_naar_vedtak_godkjennes() {
-        DigitalPostKlient digitalPostKlient = mock(DigitalPostKlient.class);
-        UtbetalingBeregner utbetalingBeregner = mock(UtbetalingBeregner.class);
-        KundeRepository kundeRepository = mock(KundeRepository.class);
-
-        VedtakService vedtakService = new VedtakService(digitalPostKlient, utbetalingBeregner, kundeRepository);
-
-        Long kundeId = 5L;
         String navn = "Ola Normann";
-
         when(utbetalingBeregner.beregnUtbetalingFor(kundeId)).thenReturn(BigDecimal.TEN);
         when(kundeRepository.getKunde(kundeId)).thenReturn(new Kunde(navn));
-
         vedtakService.godkjennVedtak(kundeId);
 
         Vedtaksbrev expected = new Vedtaksbrev(BigDecimal.TEN, navn);
 
         verify(digitalPostKlient).sendVedtaksbrev(any(UUID.class),
                 Mockito.argThat(vedtaksbrev -> Objects.equals(expected.getNavn(), vedtaksbrev.getNavn())));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void sender_ikke_digitalpost_naar_beregning_feiler() {
+        when(utbetalingBeregner.beregnUtbetalingFor(kundeId)).thenThrow(new IllegalStateException());
+
+        vedtakService.godkjennVedtak(kundeId);
+        Mockito.verifyZeroInteractions(digitalPostKlient);
+    }
+
+    @Test
+    public void sender_ikke_digitalpost_naar_0_i_utbetaling() {
+        when(utbetalingBeregner.beregnUtbetalingFor(kundeId)).thenReturn(BigDecimal.ZERO);
+
+        vedtakService.godkjennVedtak(kundeId);
+        Mockito.verifyZeroInteractions(digitalPostKlient);
     }
 
 }
