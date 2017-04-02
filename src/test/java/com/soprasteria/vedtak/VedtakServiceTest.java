f84e36f58ff19eccaa5e036a03fa60115f15b56f
teide <tee@soprasteria.com>
2017-04-02 18:58:37 +0200

    Oppgave 5 - Fikset kompileringsfeil

--- src/test/java/com/soprasteria/vedtak/VedtakServiceTest.java
+++ src/test/java/com/soprasteria/vedtak/VedtakServiceTest.java
@@ -11,20 +11,37 @@ import java.math.BigDecimal;
 import static org.mockito.Mockito.*;
 
 public class VedtakServiceTest {
+    private DigitalPostKlient digitalPostKlient = mock(DigitalPostKlient.class);
+    private UtbetalingBeregner utbetalingBeregner = mock(UtbetalingBeregner.class);
+    private KundeRepository kundeRepository = mock(KundeRepository.class);
+    private VedtakService vedtakService = new VedtakService(digitalPostKlient, utbetalingBeregner, kundeRepository);
+    private Long kundeId = 5L;
 
     @Test
     public void sender_digital_post_naar_vedtak_godkjennes() {
-        DigitalPostKlient digitalPostKlient = mock(DigitalPostKlient.class);
-        UtbetalingBeregner utbetalingBeregner = mock(UtbetalingBeregner.class);
-        KundeRepository kundeRepository = mock(KundeRepository.class);
-        VedtakService vedtakService = new VedtakService(digitalPostKlient, utbetalingBeregner, kundeRepository);
-
-        Long kundeId = 5L;
         when(utbetalingBeregner.beregnUtbetalingFor(kundeId)).thenReturn(BigDecimal.TEN);
 
         vedtakService.godkjennVedtak(kundeId);
 
 //        verify(digitalPostKlient).sendVedtaksbrev(BigDecimal.TEN);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void sender_ikke_digitalpost_naar_beregning_feiler() {
+        when(utbetalingBeregner.beregnUtbetalingFor(kundeId)).thenThrow(new IllegalStateException());
+
+        vedtakService.godkjennVedtak(kundeId);
+
+//        verify(digitalPostKlient, never()).sendVedtaksbrev(any(BigDecimal.class));
+    }
+
+    @Test
+    public void sender_ikke_digitalpost_naar_0_i_utbetaling() {
+        when(utbetalingBeregner.beregnUtbetalingFor(kundeId)).thenReturn(BigDecimal.ZERO);
+
+        vedtakService.godkjennVedtak(kundeId);
+
+//        verify(digitalPostKlient, never()).sendVedtaksbrev(any(BigDecimal.class));
     }
 
 }
